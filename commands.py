00b1a9b604fd81e0ba116a598601fe541b435602
James-F-Aus <jefriedr@gmail.com>
2025-12-20 11:56:45 +1030

    fixed typo in mirror update command

--- commands.py
+++ commands.py
@@ -11,7 +11,7 @@ maintenence_commands = {
     "del_aur_cache": ["yay", "-Sc"],
     "del_orphans": ["sudo", "pacman", "-Rns", "$(pacman -Qtdq)"],
     "clear_logs": ["sudo", "journalctl", "--vacuum-time=2weeks"],
-    "update_mirrors": ["sudo", "reflector", "--country", "Australia", "-a", "6", "--sort rate", "--save", "/etc/pacman.d/mirrorlist"],
+    "update_mirrors": ["sudo", "reflector", "--country", "AU", "-a", "6", "--sort rate", "--save", "/etc/pacman.d/mirrorlist"],
     "check_cache": ["sudo", "-du", "-sh", "~/.cache/"],
     "del_cache": ["rm", "-rf", os.path.expanduser("~/.cache/*")],
 
